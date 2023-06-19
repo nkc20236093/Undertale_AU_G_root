99cd1fa745901a7b73edfaea53e03dde8bedfe23
nkc20236093 <nck23.taki.eita@gmail.com>
2023-06-19 12:43:23 +0900

    test

---
+++ Underatale_AU/Assets/test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //by ëÍ
+        Debug.Log("ì«ÇﬂÇ‹Ç∑Ç©ÅH");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
